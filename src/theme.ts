844a8a999f86a8ff1f895f72354e91224a0b05ae
Mark D'Adamo <mark.dadamo@gmail.com>
2025-08-16 15:27:09 -0600

    starting to fill out baseline app layout

---
+++ src/theme.ts
@@ -0,0 +1,27 @@
+'use client'
+
+import { createTheme } from "@mui/material";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const theme = createTheme({
+  colorSchemes: { light: true, dark: true },
+  cssVariables: {
+    colorSchemeSelector: "class",
+  },
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+  palette: {
+    primary: {
+      main: "#000000",
+    },
+  },
+});
+
+export default theme;
