e0694640e6932861b5d1054c37801676f4880de6
Mark D'Adamo <mark.dadamo@gmail.com>
2025-08-23 20:24:35 -0600

    Logo collection from MUI template. Just uses an array map with the new Grid component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,16 @@ import { Box, Button, Container, Typography } from "@mui/material";
 import HydraLogoWords from "../../public/orange-hydra-words-trans.png";
 import Image from "next/image";
 import Hero from "@/components/Hero";
+import LogoCollection from "@/components/LogoCollection";
 
 export default function Home() {
   return (
     <div>
       <HydraAppBar />
       <Hero />
+      <div>
+        <LogoCollection />
+      </div>
       <Container maxWidth="lg">
         <Box sx={{ display: "flex", flexDirection: "column", gap: 2, alignItems: "center", justifyContent: "center", height: "100vh", width: "100%" }}>
           <Image src={HydraLogoWords} alt="Hydra Logo" width={500} height={500} />
