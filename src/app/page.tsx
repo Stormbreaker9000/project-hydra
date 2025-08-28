44af48810ae7b9ce95b79ef063b701a72149aef3
Mark D'Adamo <mark.dadamo@gmail.com>
2025-08-28 14:24:55 -0600

    added the FAQ component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import Features from "@/components/Features";
 import Highlights from "@/components/Highlights";
 import Pricing from "@/components/Pricing";
 import Footer from "@/components/Footer";
+import FAQ from "@/components/FAQ";
 
 export default function Home() {
   return (
@@ -21,6 +22,8 @@ export default function Home() {
         <Highlights />
         <Divider />
         <Pricing />
+        <Divider />
+        <FAQ />
         <Divider />
         <Footer />
       </div>
