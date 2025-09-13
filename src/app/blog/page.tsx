ab0a35bad6351684c077b39152965aa749c59822
Mark D'Adamo <mark.dadamo@gmail.com>
2025-09-13 00:19:03 -0600

    moved over the blog template

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import MainContent from './_components/MainContent';
+import Latest from './_components/Latest';
+
+
+export default function Blog(props: { disableCustomTheme?: boolean }) {
+  return (
+
+    <>
+      <Container
+        maxWidth="lg"
+        component="main"
+        sx={{ display: 'flex', flexDirection: 'column', my: 16, gap: 4 }}
+      >
+        <MainContent />
+        <Latest />
+      </Container>
+    </>
+  );
+}
