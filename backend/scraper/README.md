e9c76e31b404f1ead56713b5dc384c468b6bc800
Mark D'Adamo <mark.dadamo@gmail.com>
2025-10-05 00:33:00 -0600

    boom got a basic web scraper working

--- backend/scraper/README.md
+++ backend/scraper/README.md
@@ -1,1 +1,5 @@
 My metal archives web scraper
+
+Cursor/VS Code will complain about the python packages not being found unless you activate the `venv` and then launch with `cursor .` or `code .`
+
+Apparently the `venv` needs to be applied to the root of the editor window to make it happy
--- backend/scraper/main.py
+++ backend/scraper/main.py
@@ -1,6 +1,30 @@
-def main():
-    print("Hello from scraper!")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+import time
+
+element_list = []
 
 
-if __name__ == "__main__":
-    main()
+# Basic scraper for google.com
+
+# Set up the Chrome driver
+driver = webdriver.Chrome()
+
+try:
+    # Navigate to Google
+    driver.get("https://www.metal-archives.com")
+
+    # Wait for the page to load
+    time.sleep(2)
+
+    # find the div with id 'additional-bands' which contains a table of latest bands
+    additional_bands_div = driver.find_element(By.ID, "additionBands")
+    bands_table = additional_bands_div.find_element(By.TAG_NAME, "table")
+    rows = bands_table.find_elements(By.TAG_NAME, "tr")
+    for row in rows:
+        print(row.text)
+        element_list.append(row.text)
+
+finally:
+    driver.quit()
