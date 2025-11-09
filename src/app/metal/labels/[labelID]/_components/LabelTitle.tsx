1a22f0e0b26e9a2378311ba6600df1598aa2d494
Mark D'Adamo <mark.dadamo@gmail.com>
2025-11-09 00:58:56 -0700

    moved logo and title to their own components

--- src/app/metal/labels/[labelID]/_components/LabelTitle.tsx
+++ src/app/metal/labels/[labelID]/_components/LabelTitle.tsx
@@ -1,8 +1,10 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { Label } from "../../_data/types";
 
 export default function LabelTitle({ label }: { label: Label }) {
-    return (
-        <Typography variant="h1">{label.name}</Typography>
-    )
+  return (
+    <Box className="bg-linear-to-r from-blue-500 to-purple-500 bg-clip-text text-8xl font-black text-transparent">
+      {label.name}
+    </Box>
+  );
 }
